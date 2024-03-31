0fd087f7cde1b2228822205c14b86f8b33fef8b0
PJSanto429 <86988750+PJSanto429@users.noreply.github.com>
2024-03-31 14:50:02 -0400

    stuff n things

--- src/Components/Sides/Stats/Options/BuyAnimals.tsx
+++ src/Components/Sides/Stats/Options/BuyAnimals.tsx
@@ -2,35 +2,51 @@ import React, { useCallback, useState } from 'react'
 
 export const BuyAnimals = (props: {
     showBuyAnimals: boolean,
-    setShowBuyAnimals: (show: boolean) => void
+    setShowBuyAnimals: (show: boolean) => void,
+    handleBuyAnimals: (animal: string, amount: number) => void
 }) => {
     const {
         showBuyAnimals,
-        setShowBuyAnimals
+        setShowBuyAnimals,
+        handleBuyAnimals
     } = props
 
     const [animalToBuy, setAnimalToBuy] = useState<string>()
-    const [amount, setAmount] = useState<number>()
+    const [amount, setAmount] = useState<number>(0)
 
     const [showConfirm, setShowConfirm] = useState(false)
+
+    const buyAnimals = useCallback(() => {
+        if (animalToBuy !== undefined) {
+            handleBuyAnimals(animalToBuy, amount)
+        }
+    }, [animalToBuy, amount, handleBuyAnimals])
 
     const Confirm = useCallback(() => {
         if (!showConfirm) {
             return (
-                <button onClick={() => {setShowConfirm(true)}}>Buy</button>
+                <button
+                    onClick={() => {
+                        setShowConfirm(true)
+                    }}
+                >
+                    Buy
+                </button>
             )
         }
         return (
-            <button onClick={() => {}}>
-                confirming...
+            <button onClick={() => {buyAnimals()}}>
+                Press again to confirm
             </button>
         )
-    }, [showConfirm])
+    }, [showConfirm, buyAnimals])
 
     if (showBuyAnimals) {
         return (
             <>
-                <select onChange={(e) => {setAnimalToBuy(e.target.value)}}>
+                <label htmlFor='animal'>Animal:</label>
+                <select onChange={(e) => {setAnimalToBuy(e.target.value)}} name='animal'>
+                    <option value=''>Select...</option>
                     <option value="chicken">Chicken</option>
                     <option value="chicken">Cow</option>
                     <option value="chicken">Cat</option>
@@ -41,6 +57,7 @@ export const BuyAnimals = (props: {
                 <input
                     name='amount'
                     type='number'
+                    min={0}
                     placeholder='5...'
                     onChange={
                         (e) => {
--- src/Components/Sides/Stats/Options/Options.tsx
+++ src/Components/Sides/Stats/Options/Options.tsx
@@ -10,12 +10,22 @@ export const Options = (props: {
     } = props
 
     const [showBuyAnimals, setShowBuyAnimals] = useState(true)
+    
+    const handleBuyAnimals = (
+        animalToBuy: string,
+        amount: number
+    ) => {
+        console.clear()
+        console.log("animal ==> ", animalToBuy)
+        console.log("amount ==> ", amount)
+    }
 
     return (
         <>
             <BuyAnimals
                 showBuyAnimals={showBuyAnimals}
                 setShowBuyAnimals={setShowBuyAnimals}
+                handleBuyAnimals={handleBuyAnimals}
             />
         </>
     )
